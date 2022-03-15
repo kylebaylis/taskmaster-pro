58cd8878ede815fc7f0dccf9cd06e7321fba7b77
Kyle <kylejmbaylis@gmail.com>
2022-03-14 20:46:13 -0400

    fixed indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,24 +63,24 @@ $(".list-group").on("blur", "textarea", function() {
     .val()
     .trim();
   
-    // get the parent ul's id attribute
-    var status = $(this)
-      .closest(".list-group")
-      .attr("id")
-      .replace("list-", "");
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
 
-    // get the task's position in the list of other li elements
-    var index = $(this)
-      .closest(".list-group-item")
-      .index();
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
 
-    // recreate p element
-    var taskP = $("<p>")
-      .addClass("m-1")
-      .text(text);
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
 
-    // replace textarea with p element
-    $(this).replaceWith(taskP);
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
 });
 
 // modal was triggered
